9e39a3b653d2fa351ee54eee5fe135117f60f1fb
201924496 <moonlight6750@naver.com>
2023-06-27 16:13:11 +0900

    세부 사항 수정 작업 임시저장

--- Assets/GameStartScripts/GameStart.cs
+++ Assets/GameStartScripts/GameStart.cs
@@ -6,20 +6,23 @@ using UnityEngine.SceneManagement;
 public class GameStart : MonoBehaviour
 {
     public static bool newGame;
+    public GameObject settingWindow;
+    public GameObject soundSetting;
+    private bool pause = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        newGame=true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        checkPause();
     }
 
     public void onStartClick(){
-        newGame=true;
         SceneManager.LoadScene("Loading");
     }
 
@@ -37,5 +40,31 @@ public class GameStart : MonoBehaviour
 #else
         Application.Quit(); // 어플리케이션 종료
 #endif
+    }
+
+    public void onClickContinue(){
+        pause=false;
+        Time.timeScale=1;
+        settingWindow.SetActive(false);
+    }
+
+    public void checkPause(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(!pause)
+            {
+                pause=true;
+                Time.timeScale=0;
+                settingWindow.SetActive(true);
+                
+            }
+            else
+            {
+                pause=false;
+                Time.timeScale=1;
+                soundSetting.SetActive(false);
+                settingWindow.SetActive(false);
+                
+            }
+        }
     }
 }
--- Assets/Scripts/SettingWindow.cs
+++ Assets/Scripts/SettingWindow.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingWindow : MonoBehaviour
 {
     [SerializeField] private GameObject inventory;
     [SerializeField] private GameObject settingWindow;
     [SerializeField] private GameObject soundSetting;
+    
+    public Slider BG;
+    public Slider SFX;
+
+    [SerializeField] private Text BGp;
+    [SerializeField] private Text SFXp;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +23,17 @@ public class SettingWindow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        BGp.text=(BG.value*0.5*100).ToString("F1")+"%";
+        SFXp.text=(SFX.value*0.5*100).ToString("F1")+"%";
     }
 
     public void onClickContinue(){
         GameObject.Find("GameManager").GetComponent<GameManager>().pause=false;
         Time.timeScale=1;
-        inventory.SetActive(true);
+        if(inventory!=null){
+            inventory.SetActive(true);
+        }
+        
         settingWindow.SetActive(false);
     }
     public void onClickSetting(){
