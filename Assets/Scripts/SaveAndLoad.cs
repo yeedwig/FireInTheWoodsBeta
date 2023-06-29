ceb7bfa569def615dc9fe18410c71379f7e3139d
201924496 <moonlight6750@naver.com>
2023-06-29 19:36:22 +0900

    세이브 로드 완료
    메인 캐릭터 세이브 부분 확인되면 세이브할 예정

--- Assets/Scripts/SaveAndLoad.cs
+++ Assets/Scripts/SaveAndLoad.cs
@@ -18,9 +18,20 @@ public class SaveAndLoad : MonoBehaviour
     public GameManager gm;
     public GameManager gmTemp;
 
-
-
+    //캐릭터 저장
     [SerializeField] private GameObject mainCharacter;
+    public MainCharacter mc;
+    public MainCharacter mcTemp;
+
+    //아이템북 저장
+    [SerializeField] private GameObject itemBook;
+    public ItemBook ib;
+    public ItemBook ibTemp;
+
+    //아이템 저장
+    [SerializeField] private GameObject[] items;
+    public Slot item;
+    public Slot itemTemp;
 
 
     // Start is called before the first frame update
@@ -28,19 +39,21 @@ public class SaveAndLoad : MonoBehaviour
     {
         animals = Animal.GetComponentsInChildren<Animals>(true);
         gm = gameManager.GetComponent<GameManager>();
+        mc = mainCharacter.GetComponent<MainCharacter>();
+        ib = itemBook.GetComponent<ItemBook>();
         if(!GameStart.newGame){
+            GameStart.newGame=true;
             Load();
         }
-        
     }
 
-    
     public void Save()
     {
         StartCoroutine(SaveAnimals());
         StartCoroutine(SaveGameManager());
-        //StartCoroutine(SaveMainCharacter());
-        
+        //StartCoroutine(SaveMainCharacter()); 
+        StartCoroutine(SaveItemBook());
+        StartCoroutine(SaveItems());
     }
 
     IEnumerator SaveAnimals(){
@@ -48,8 +61,7 @@ public class SaveAndLoad : MonoBehaviour
             string json = JsonUtility.ToJson(animals[i]);
             string fileName=animals[i].name;
             string path = Application.dataPath + "/GameData/" + fileName + ".Json";
-            File.WriteAllText(path,json);
-            
+            File.WriteAllText(path,json);  
         }
         yield return null;
     }
@@ -70,11 +82,31 @@ public class SaveAndLoad : MonoBehaviour
         yield return null;
     }*/
 
+    IEnumerator SaveItemBook(){
+        string json = JsonUtility.ToJson(ib);
+        string fileName="ItemBook";
+        string path = Application.dataPath + "/GameData/" + fileName + ".Json";
+        File.WriteAllText(path,json);
+        yield return null;
+    }
+
+    IEnumerator SaveItems(){
+        for(int i=0;i<items.Length;i++){
+            string json = JsonUtility.ToJson(items[i].GetComponent<Slot>());
+            string fileName="Slot"+i.ToString();
+            string path = Application.dataPath + "/GameData/" + fileName + ".Json";
+            File.WriteAllText(path,json);  
+        }
+        yield return null;
+    }
+
     void Load()
     {
         StartCoroutine(LoadAnimals());
         StartCoroutine(LoadGameManager());
         //StartCoroutine(LoadMainCharacter());
+        StartCoroutine(LoadItemBook());
+        StartCoroutine(LoadItems());
     }
 
     IEnumerator LoadAnimals(){
@@ -104,9 +136,34 @@ public class SaveAndLoad : MonoBehaviour
     }
 
     /*IEnumerator LoadMainCharacter(){
-        
+        string path = Application.dataPath+"/GameData/"+"MainCharacter"+".json";
+        string json = File.ReadAllText(path);
+        mcTemp=new MainCharacter();
+        JsonUtility.FromJsonOverwrite(json,mcTemp);
+        mc.cameraMode=mcTemp.cameraMode;
         yield return null;
     }*/
+
+    IEnumerator LoadItemBook(){
+        string path = Application.dataPath+"/GameData/"+"ItemBook"+".json";
+        string json = File.ReadAllText(path);
+        ibTemp=new ItemBook();
+        JsonUtility.FromJsonOverwrite(json,ibTemp);
+        ib.foundCombinations=ibTemp.foundCombinations;
+
+        yield return null;
+    }
+
+    IEnumerator LoadItems(){
+        for(int i=0;i<items.Length;i++){
+            string path = Application.dataPath+"/GameData/"+"Slot"+i.ToString()+".json";
+            string json = File.ReadAllText(path);
+            itemTemp=new Slot();
+            JsonUtility.FromJsonOverwrite(json,itemTemp);
+            items[i].GetComponent<Slot>().AddItem(itemTemp.item,itemTemp.itemCount);
+        }
+        yield return null;
+    }
 
    
 }
